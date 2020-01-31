08915ee5f870374d76e18bdf04377c105884b908
Ethan Kopf <ethan.kopf@gmail.com>
2020-01-31 12:38:05 -0500

    Initial Commit

---
+++ Museify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Museify
+//
+//  Created by Ethan Kopf on 1/31/20.
+//  Copyright © 2020 Ethan Kopf. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
