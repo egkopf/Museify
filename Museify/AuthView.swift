cdce6740f4898b1fb3b436a997672895aaf16bb0
Ethan Kopf <ethan.kopf@gmail.com>
2020-02-10 21:32:33 -0500

    second try, adding auth files

---
+++ Museify/AuthView.swift
@@ -0,0 +1,24 @@
+//
+//  AuthView.swift
+//  Museify
+//
+//  Created by Ethan Kopf on 2/10/20.
+//  Copyright © 2020 Ethan Kopf. All rights reserved.
+//
+
+import Foundation
+
+struct SigningInView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var error: String = ""
+    @EnvironmentObject var auth: Authentication
+    
+    func signIn() {
+        auth.signIn(email: email, password: password) { (result, error) in
+            if let error = error {
+                self.error = error.localizedDescription
+            }
+        }
+    }
+}
