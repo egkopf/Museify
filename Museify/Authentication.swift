5ac86f712f6a8b08294fd104ab47795a4bb93fdf
Ethan Kopf <ethan.kopf@gmail.com>
2020-02-10 21:26:27 -0500

    Added Authentication.swift

---
+++ Museify/Authentication.swift
@@ -0,0 +1,68 @@
+//
+//  Authentication.swift
+//  Museify
+//
+//  Created by Ethan Kopf on 2/10/20.
+//  Copyright © 2020 Ethan Kopf. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+import Combine
+
+class Authentication: ObservableObject {
+    @Published var currentUser: User?
+    
+    var handle: AuthStateDidChangeListenerHandle?
+    
+    func listen() {
+        handle = Auth.auth().addStateDidChangeListener({ (auth, user) in
+            if let user = user {
+                self.currentUser = User(uid: user.uid, email: user.email)
+            } else {
+                self.currentUser = nil
+            }
+        })
+    }
+    
+    func signUp(email: String, password: String, handler: @escaping AuthDataResultCallback) {
+        Auth.auth().createUser(withEmail: email, password: password, completion: handler)
+    }
+    
+    func signIn(email: String, password: String, handler: @escaping AuthDataResultCallback) {
+        Auth.auth().signIn(withEmail: email, password: password, completion: handler)
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+            self.currentUser = nil
+        } catch {
+            print("Error signing out")
+        }
+    }
+    
+    func unbind() {
+        if let handle = handle {
+            Auth.auth().removeStateDidChangeListener(handle)
+        }
+    }
+    
+    deinit {
+        unbind()
+    }
+}
+
+struct User {
+    var uid: String
+    var email: String?
+    
+    init(uid: String, email: String?) {
+        self.uid = uid
+        self.email = email
+    }
+}
+
+
+
