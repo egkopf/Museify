2b6ced6565ce2470d18ffbc645f9055666ff9b74
Ethan Kopf <ethan.kopf@gmail.com>
2020-04-12 16:05:54 -0400

    Search bar now works! keep up the progress

--- Museify/SearchingOrParticipatingInHunt/Search.swift
+++ Museify/SearchingOrParticipatingInHunt/Search.swift
@@ -17,13 +17,55 @@ import FirebaseFirestore
 import FirebaseFirestoreSwift
 import FirebaseStorage
 
-class Hunt: Identifiable, Equatable, CustomStringConvertible {
+
+struct SearchBar: UIViewRepresentable {
+
+    @Binding var text: String
+    var placeholder: String
+
+    class Coordinator: NSObject, UISearchBarDelegate {
+
+        @Binding var text: String
+
+        init(text: Binding<String>) {
+            _text = text
+        }
+
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+        }
+    }
+
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        searchBar.placeholder = placeholder
+        searchBar.searchBarStyle = .minimal
+        searchBar.autocapitalizationType = .none
+        return searchBar
+    }
+
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
+    }
+}
+
+
+class Hunt: Identifiable, Equatable, CustomStringConvertible, Hashable {
     static func == (lhs: Hunt, rhs: Hunt) -> Bool {
         return lhs.name == lhs.name && lhs.description == lhs.description
     }
     
     var name: String
     var description: String
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
     
     init(name: String, description: String) {
         self.name = name
@@ -34,13 +76,11 @@ class Hunt: Identifiable, Equatable, CustomStringConvertible {
 struct Search: View {
     var db = Firestore.firestore()
     @State public var hunts = [Hunt]()
-    @State public var searchBar = "A"
+    @State public var searchBar = ""
     
     func getAllHunts() {
         db.collection("hunts").getDocuments() { (querySnapshot, err) in
-            if let err = err {
-                print("Error getting documents: \(err)")
-            } else {
+            if let err = err {print("Error getting documents: \(err)")} else {
                 for document in querySnapshot!.documents {
                     let newHunt = Hunt(name: document.data()["name"] as! String, description: document.data()["description"] as! String)
                     for hunt in self.hunts {if hunt.name == newHunt.name {return}}
@@ -48,30 +88,23 @@ struct Search: View {
                 }
             }
         }
-        self.hunts.filter{$0.name.contains("\(self.$searchBar)")}
-
-    }
-    
-    func getAllHuntsAndClear() {
-        self.hunts = [Hunt]()
-        self.getAllHunts()
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                Text("💙Hunts💙").font(.largeTitle)
-                HStack {
-                    Text("Search:")
-                    TextField("Search for Hunt", text: $searchBar, onEditingChanged: { _ in self.getAllHuntsAndClear()})
-                }
-                List(self.hunts) { hunt in
-                    NavigationLink(destination: HuntStops(name: hunt.name)) {
-                        HuntRow(name: hunt.name, description: hunt.description)
+                SearchBar(text: $searchBar, placeholder: "Search")
+                List {
+                    ForEach(self.hunts) { hunt in
+                        if hunt.name.contains(self.searchBar) || hunt.description.contains(self.searchBar) || self.searchBar == "" {
+                            NavigationLink(destination: HuntStops(name: hunt.name)) {
+                                HuntRow(name: hunt.name, description: hunt.description)
+                            }
+                        }
                     }
                 }.onAppear {
                     self.getAllHunts()
-                }
+                }.navigationBarTitle(Text("Hunts"))
             }
         }
     }
