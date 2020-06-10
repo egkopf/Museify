c559617582b02e6a1fe6b68a47bddaad2cec72a9
Aron Korsunsky <aronkorsunsky@gmail.com>
2020-06-10 14:25:41 -0400

    worked on search

--- Museify/SearchingOrParticipatingInHunt/Search.swift
+++ Museify/SearchingOrParticipatingInHunt/Search.swift
@@ -20,27 +20,27 @@ import CoreLocation
 
 
 struct SearchBar: UIViewRepresentable {
-
+    
     @Binding var text: String
     var placeholder: String
-
+    
     class Coordinator: NSObject, UISearchBarDelegate {
-
+        
         @Binding var text: String
-
+        
         init(text: Binding<String>) {
             _text = text
         }
-
+        
         func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
             text = searchText
         }
     }
-
+    
     func makeCoordinator() -> SearchBar.Coordinator {
         return Coordinator(text: $text)
     }
-
+    
     func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
         let searchBar = UISearchBar(frame: .zero)
         searchBar.delegate = context.coordinator
@@ -49,7 +49,7 @@ struct SearchBar: UIViewRepresentable {
         searchBar.autocapitalizationType = .none
         return searchBar
     }
-
+    
     func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
         uiView.text = text
     }
@@ -63,6 +63,8 @@ class Hunt: Identifiable, Equatable, CustomStringConvertible, Hashable {
     var name: String
     var description: String
     var key: Int?
+    var stops: [Stop]
+    var closestStop: Double
     
     var id: String { name }
     
@@ -70,16 +72,18 @@ class Hunt: Identifiable, Equatable, CustomStringConvertible, Hashable {
         hasher.combine(name)
     }
     
-    init(name: String, description: String, key: Int?) {
+    init(name: String, description: String, key: Int?, stops: [Stop], closestStop: Double) {
         self.name = name
         self.description = description
         self.key = key
+        self.stops = stops
+        self.closestStop = closestStop
     }
 }
 
 struct Search: View {
     var db = Firestore.firestore()
-    @State public var hunts = [Hunt : Double]()
+    @State public var hunts = [Hunt]()
     @State public var searchBar = ""
     @State var privKey: String = ""
     @State var currentHuntName = ""
@@ -102,47 +106,54 @@ struct Search: View {
         return Double(meters / 16.0934).rounded() / 100
     }
     
-    func calculateClosestStop(hunt: String, currLat: Double, currLon: Double) -> Double {
+    func calculateClosestStop(stops: [Stop], currLat: Double, currLon: Double) -> Double {
         var distances = [Double]()
-        db.collection("hunts").document("\(hunt)").collection("stops").getDocuments() { (querySnapshot, err) in
-            if let err = err {print("Error getting documents: \(err)")} else {
-                for document in querySnapshot!.documents {
-                    let stopLat = document.data()["latitude"] as! Double
-                    let stopLon = document.data()["longitude"] as! Double
-                    distances.append(Double(CLLocation(latitude: currLat, longitude: currLon).distance(from: CLLocation(latitude: stopLat, longitude: stopLon))))
-                }
-            }
+        for stop in stops {
+            distances.append(Double(CLLocation(latitude: currLat, longitude: currLon).distance(from: CLLocation(latitude: stop.latitude, longitude: stop.longitude))))
         }
         return distances.min()!
+        //return 10.0
     }
-
+    
     
     func getAllHunts() {
         db.collection("hunts").getDocuments() { (querySnapshot, err) in
             if let err = err {print("Error getting documents: \(err)")} else {
                 for document in querySnapshot!.documents {
-                    let newHunt = Hunt(name: document.data()["name"] as! String, description: document.data()["description"] as! String, key: (document.data()["huntID"] as? Int))
-                    for (hunt, distance) in self.hunts {if hunt.name == newHunt.name {return}}
-                    self.hunts[newHunt] = self.calculateClosestStop(hunt: newHunt.name, currLat: self.userLatitude, currLon: self.userLongitude)
-                    if querySnapshot!.documents.count == self.hunts.count {self.getAllImages()}
+                    var huntStops = [Stop]()
+                    var closestStop = Double()
+                    self.db.collection("hunts").document("\(document.data()["name"] as! String)").collection("stops").getDocuments() { (querySnapshotTwo, errTwo) in
+                        if let errTwo = errTwo {print("Error getting documents: \(errTwo)")} else {
+                            for documentTwo in querySnapshotTwo!.documents {
+                                huntStops.append(Stop(Name: documentTwo.data()["name"] as! String, StopDescription: documentTwo.data()["description"] as! String, ImgName: documentTwo.data()["imageName"] as! String, Latitude: documentTwo.data()["latitude"] as! Double, Longitude: documentTwo.data()["longitude"] as! Double, Direction: documentTwo.data()["direction"] as! Double))
+                            }
+                            if querySnapshotTwo!.documents.count != 0 {
+                                closestStop = self.calculateClosestStop(stops: huntStops, currLat: self.userLatitude, currLon: self.userLongitude)
+                            }
+                            print(huntStops)
+                        }
+                    }
+                    let newHunt = Hunt(name: document.data()["name"] as! String, description: document.data()["description"] as! String, key: (document.data()["huntID"] as? Int), stops: huntStops, closestStop: closestStop)
+                    self.hunts.append(newHunt)
                 }
+                if querySnapshot!.documents.count == self.hunts.count {self.getAllImages()}
             }
         }
-        // print("hunts: \(hunts)")
+        print("hunts: \(hunts)")
     }
     
     func getAllImages() {
         let tempHunts = hunts.filter({$0.key == nil})
         if tempHunts.count == images.count {return}
         
-        for (hunt, distance) in hunts {
+        for hunt in hunts {
             if hunt.key == nil {
                 let storageRef = Storage.storage().reference()
                 let imgRef = storageRef.child("images/\(hunt.name)CoverImage")
                 
                 imgRef.getData(maxSize: 1 * 8000 * 8000) { data, error in
                     if let theError = error {print(theError); return}
-                    print("no error")
+                    //print("no error")
                     self.images[hunt.name] = UIImage(data: data!)!
                 }
             }
@@ -153,35 +164,35 @@ struct Search: View {
     func doNothing() {}
     
     func getPrivHunt() {
-        for (hunt, distance) in self.hunts {
+        for hunt in self.hunts {
             if hunt.key == Int(privKey) {
                 self.currentHuntName = hunt.name
                 ready = true
             }
         }
-        print("no hunt")
+        //print("no hunt")
     }
     
-    func getCompletedStops() {
-        let stopRef = db.collection("users").document("\(auth.currentEmail!)").collection("stopsCompleted")
-        
-        stopRef.getDocuments() { (querySnapshot, err) in
-            if let err = err {
-                print("Error getting documents: \(err)")
-            } else {
-                for document in querySnapshot!.documents {
-                    //if (document.data()["name"] as! String).hasPrefix("\(self.name)") {
-                        //print(document.data())
-                        self.completedStops.append(document.data()["name"] as! String)
-                        //print(self.stops)
-                    //}
-                }
-            }
-        }
-    }
+    /*func getCompletedStops() {
+     let stopRef = db.collection("users").document("\(auth.currentEmail!)").collection("stopsCompleted")
+     
+     stopRef.getDocuments() { (querySnapshot, err) in
+     if let err = err {
+     print("Error getting documents: \(err)")
+     } else {
+     for document in querySnapshot!.documents {
+     //if (document.data()["name"] as! String).hasPrefix("\(self.name)") {
+     //print(document.data())
+     self.completedStops.append(document.data()["name"] as! String)
+     //print(self.stops)
+     //}
+     }
+     }
+     }
+     }*/
     
     
-
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -200,67 +211,67 @@ struct Search: View {
                 
                 List {
                     VStack {
-                        ForEach(Array(self.hunts.keys).sorted(by: {hunts[$0]! < hunts[$1]!})) { hunt in
+                        ForEach(hunts.sorted(by: { $0.closestStop < $1.closestStop })) { hunt in
                             if (hunt.name.lowercased().contains(self.searchBar.lowercased()) || hunt.description.lowercased().contains(self.searchBar.lowercased()) || self.searchBar == "") && (hunt.key == nil) {
-                            NavigationLink(destination: HuntStops(name: hunt.name)) {
-                                HStack {
-                                    VStack(alignment: .leading) {
-                                        Text("\(hunt.name)").font(.custom("Averia-Bold", size: 18))
-                                        Text("\(hunt.description)")
-                                
-                                    }.font(.custom("Averia-Regular", size: 18))
-                                    if self.completedStops.contains { $0.hasPrefix("\(hunt)") } {
-                                        Text("Underway").foregroundColor(.green).font(.custom("Averia-Bold", size: 16))
-                                    }
-                                    Spacer()
-                                    Text("Closest Stop: \(self.metersToMiles(meters: self.hunts[hunt]!)) miles away").font(.custom("Averia-Bold", size: 12))
-                                    Spacer()
-                                    if self.images[hunt.name] != nil {
-                                        Image(uiImage: self.images[hunt.name]!).resizable()
-                                            .frame(width: 50, height: 50, alignment: .trailing).clipShape(RoundedRectangle(cornerRadius: 10))
+                                NavigationLink(destination: HuntStops(name: hunt.name)) {
+                                    HStack {
+                                        VStack(alignment: .leading) {
+                                            Text("\(hunt.name)").font(.custom("Averia-Bold", size: 18))
+                                            Text("\(hunt.description)")
+                                            
+                                        }.font(.custom("Averia-Regular", size: 18))
+                                        if self.completedStops.contains { $0.hasPrefix("\(hunt)") } {
+                                            Text("Underway").foregroundColor(.gray).font(.custom("Averia-Bold", size: 16))
+                                        }
+                                        Spacer()
+                                        Text("Closest Stop: \(self.metersToMiles(meters: hunt.closestStop), specifier: "%.2f") miles away").font(.custom("Averia-Bold", size: 12))
+                                        Spacer()
+                                        if self.images[hunt.name] != nil {
+                                            Image(uiImage: self.images[hunt.name]!).resizable()
+                                                .frame(width: 50, height: 50, alignment: .trailing).clipShape(RoundedRectangle(cornerRadius: 10))
+                                            
+                                        }
                                         
                                     }
-                                    
                                 }
                             }
                         }
+                        
                     }
                     
-                }
-            
-                
-                VStack {
-                    HStack {
-                        Text("Enter a Private Hunt Key:")
-                        TextField("enter", text: $privKey).frame(width: 60)
-                        Button(action: self.getPrivHunt) {
-                            Text("find")
-                        }
-                        VStack {
-                            if currentHuntName != "" {
-                                Text(currentHuntName)
+                    
+                    VStack {
+                        HStack {
+                            Text("Enter a Private Hunt Key:")
+                            TextField("enter", text: $privKey).frame(width: 60)
+                            Button(action: self.getPrivHunt) {
+                                Text("find")
+                            }
+                            VStack {
+                                if currentHuntName != "" {
+                                    Text(currentHuntName)
+                                }
+                                
+                                NavigationLink(destination: HuntStops(name: currentHuntName)) {
+                                    Text("embark")
+                                }.disabled(currentHuntName == "")
                             }
                             
-                            NavigationLink(destination: HuntStops(name: currentHuntName)) {
-                                Text("embark")
-                            }.disabled(currentHuntName == "")
-                        }
-                        
-                        
-                        
-                    }.padding()
-                }.background(RoundedRectangle(cornerRadius: 25, style: .continuous).fill(Color.blue).opacity(0.12), alignment: .bottom)
-                Spacer().frame(height: 20)
-            }.frame(width: 400)
-            .onAppear { self.getAllHunts() }
-            .onAppear { self.getCompletedStops() }
-        }.font(.custom("Averia-Regular", size: 18))
+                            
+                            
+                        }.padding()
+                    }.background(RoundedRectangle(cornerRadius: 25, style: .continuous).fill(Color.blue).opacity(0.12), alignment: .bottom)
+                    Spacer().frame(height: 20)
+                }.frame(width: 400)
+                    .onAppear { self.getAllHunts() }
+                //   .onAppear { self.getCompletedStops() }
+            }.font(.custom("Averia-Regular", size: 18))
+        }
     }
-}
-
-struct Search_Previews: PreviewProvider {
-    static var previews: some View {
-        Search()
+    
+    struct Search_Previews: PreviewProvider {
+        static var previews: some View {
+            Search()
+        }
     }
-}
 }
