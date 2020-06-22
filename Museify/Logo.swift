1522a570fd661280d686ecfdbfd96a876db604f5
Aron Korsunsky <aronkorsunsky@gmail.com>
2020-06-21 20:05:23 -0400

    oops committed out smt i shouldn't have

--- Museify/Logo.swift
+++ Museify/Logo.swift
@@ -1,28 +1,28 @@
-////
-////  Logo.swift
-////  Museify
-////
-////  Created by Ethan Kopf on 2/14/20.
-////  Copyright © 2020 Ethan Kopf. All rights reserved.
-////
 //
-//import SwiftUI
+//  Logo.swift
+//  Museify
 //
-//struct Logo: View {
-//    var body: some View {
-//        ZStack {
-//            Image("M").resizable()
-//                .clipShape(Circle())
-//                .overlay(
-//                    Circle().stroke(Color.white, lineWidth: 4))
-//                .shadow(radius: 5)
-//                .aspectRatio(contentMode: .fit)
-//        }
-//    }
-//}
+//  Created by Ethan Kopf on 2/14/20.
+//  Copyright © 2020 Ethan Kopf. All rights reserved.
 //
-//struct Logo_Previews: PreviewProvider {
-//    static var previews: some View {
-//        Logo()
-//    }
-//}
+
+import SwiftUI
+
+struct Logo: View {
+    var body: some View {
+        ZStack {
+            Image("M").resizable()
+                .clipShape(Circle())
+                .overlay(
+                    Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(radius: 5)
+                .aspectRatio(contentMode: .fit)
+        }
+    }
+}
+
+struct Logo_Previews: PreviewProvider {
+    static var previews: some View {
+        Logo()
+    }
+}
